737d765f620c5d8f6cf175e1a8f11fbbbc55f0a5
pritamg7 <pritampg774826@gmail.com>
2021-11-25 23:52:36 +0530

    third commit

--- results/models.py
+++ results/models.py
@@ -7,8 +7,8 @@ from django.contrib.auth.models import User
 class Result(models.Model):
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    stuname = models.CharField(max_length=20)
-    mail = models.CharField(max_length=20)
+    # stuname = models.CharField(max_length=20)
+    # mail = models.CharField(max_length=20)
     score = models.FloatField()
 
     def __str__(self):
