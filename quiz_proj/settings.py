dac6ccecf1f27e59a905c04bd9329e3348e26f5c
pritamg7 <pritampg774826@gmail.com>
2021-11-26 09:33:24 +0530

    sixth commit

--- quiz_proj/settings.py
+++ quiz_proj/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import django_heroku
 import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS= (os.path.join(BASE_DIR,'static'),)
 STATICFILES_DIRS=[
     BASE_DIR/'static',
     BASE_DIR/'quizes'/'static',
